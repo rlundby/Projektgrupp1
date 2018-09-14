42957cda65c16b1064b2a1b32f001afffc528810
linneaagetorp <linnea.agetorp@gmail.com>
2018-09-14 14:13:35 +0200

    react-delen av inbox

--- html/projektarbete/src/App.js
+++ html/projektarbete/src/App.js
@@ -1,15 +1,17 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import LatestTransactions from './components/latestTransactions';
+import Inbox from './components/inbox';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <LatestTransactions/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Inbox/>
+                <LatestTransactions/>
+            </div>
+        );
+    }
 }
 
 export default App;
