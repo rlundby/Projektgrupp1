b577c6e2bbc7285a3c6b96a36b07ffacb3c7a213
Ellinor Danielsson <ellinor0926@hotmail.com>
2018-09-14 11:01:02 +0200

    Added getProductWithId function and gave it a route

--- api/controllers/productsController.js
+++ api/controllers/productsController.js
@@ -12,3 +12,16 @@ exports.getProducts = (req, res) => {
             res.status(500).json({ error })
         })
 };
+
+exports.getProductWithId = (req, res) => {
+    Product.findById(req.params.id)
+        .select('_id name price')
+        .exec()
+        .then(product => res.status(200).json(product))
+        .catch(() => {
+            res.status(500).json({
+                errorCode: '500',
+                message: 'Something went wrong, please try again later'
+            })
+        });
+};
