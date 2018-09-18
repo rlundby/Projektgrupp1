83c1febde843ac348190a3302be63980b90d7f23
rlundby <rickardlundby@gmail.com>
2018-09-18 21:30:29 +0200

    updated the transactions component with fixes

--- html/projektarbete/src/components/latestTransactions/index.js
+++ html/projektarbete/src/components/latestTransactions/index.js
@@ -1,5 +1,7 @@
 
 import React, { Component } from "react";
+import EditingTransactions from "../editingTransactions";
+import TransactionStatus from "../transactionStatus";
 
 class LatestTransactions extends Component {
 
@@ -12,7 +14,9 @@ class LatestTransactions extends Component {
     componentDidMount() {
         fetch('http://localhost:3001/api/transactions')
             .then(response => response.json())
-            .then(transactions => this.setState({ transactions }));
+            .then(transactions => {
+                this.setState({ transactions: transactions.reverse() });
+            });
     }
 
     getUpdatedTransactions() {
@@ -62,15 +66,13 @@ class LatestTransactions extends Component {
     }
     };
 
-    handleSubmit = event => {
-        console.log('Form submitting!');
+    handleSubmit (){
         this.updateTransaction();
-        event.preventDefault();
     };
 
     updateTransaction(){
         console.log('You are trying to update:');
-        console.log(this.state.currentTransaction)
+        console.log(this.state.currentTransaction);
         fetch('http://localhost:3001/api/update-transaction/' + this.state.currentTransaction._id, {
             method: 'PUT',
             headers: {
@@ -83,7 +85,7 @@ class LatestTransactions extends Component {
                 console.log(res);
                 this.setState({
                     editing: false
-                })
+                });
                 this.getUpdatedTransactions();
             })
             .catch(err => console.log(err))
@@ -93,147 +95,61 @@ class LatestTransactions extends Component {
     render() {
         if(!this.state.transactions) {
             return(
-                <div>No transactions found</div>
+                <div>No transactions found. Did you start the server?</div>
             )
         }else if(this.state.editing) {
             return (
-
-                
-                        <div className="col-xl-6">
-                            <div className="card m-b-20">
-                                <div className="card-body">
-                                    <h4 className="mt-0 m-b-30 header-title">Editing transaction: {this.state.currentTransaction._id}</h4>
-                                    <form onSubmit={this.handleSubmit}>
-                                        <div className="form-group">
-                                            <label htmlFor="name">Name:</label>
-                                            <input className="form-control" name="name" type="text" value={this.state.currentTransaction.name} onChange={this.handleChange}/>
-                                        </div>
-                                        <div className="form-group">
-                                            <label htmlFor="status">Status:</label>
-                                            <select className="form-control" name="status" value={this.state.currentTransaction.status} onChange={this.handleChange}>
-                                                <option disabled value=''>  Select a status </option>
-                                                <option value="Confirmed">Payment confirmed</option>
-                                                <option value="Waiting">Payment waiting</option>
-                                                <option value="Expired">Payment declined</option>
-                                            </select>
-                                        </div>
-                                        <div className="form-group">
-                                            <label htmlFor="name">Amount:</label>
-                                            <input className="form-control" name="amount" type="number" value={this.state.currentTransaction.amount} onChange={this.handleChange}/>
-                                        </div>
-                                        <button type="submit" className="btn btn-lg btn-success">Submit changes</button>
-                                    </form>
-                                    <button className="btn btn-lg mt-3" onClick={() => {this.setState({editing: false})}}>Cancel</button>
-                                </div>
-                            </div>
-                        </div>
-                  
-
+                <EditingTransactions
+                    currentTransaction={this.state.currentTransaction}
+                    onChange={this.handleChange}
+                    onSubmit={() => this.handleSubmit()}
+                    onCancel={() => {this.setState({editing: false})}}
+                />
             )
         } else {
             return (
-                    
-                        <div className="col-xl-6">
-                            <div className="card m-b-20">
-                                <div className="card-body">
-                                    <h4 className="mt-0 m-b-30 header-title">Latest Transactions</h4>
-
-                                    <div className="table-responsive">
-                                        <table className="table table-vertical">
-                                            <tbody>
-                                            {this.state.transactions.map((transaction, i) => {
-                                                if(transaction.status === 'Confirmed') {
-                                                    return (
-                                                        <tr key={i}>
-                                                            <td>
-                                                                <img src="assets/images/users/user-2.jpg" alt="user"
-                                                                     className="thumb-sm rounded-circle mr-2"/>
-                                                                {transaction.name}
-                                                            </td>
-
-
-                                                            <td><i className="mdi mdi-checkbox-blank-circle text-success"></i> Confirm</td>
-                                                            <td>
-                                                                ${transaction.amount}
-                                                                <p className="m-0 text-muted font-14">Amount</p>
-                                                            </td>
-                                                            <td>
-                                                                {new Date(transaction.date).toISOString().split('T')[0]}
-                                                                <p className="m-0 text-muted font-14">Date</p>
-                                                            </td>
-                                                            <td>
-                                                                <button type="button"
-                                                                        className="btn btn-secondary btn-sm waves-effect waves-light"
-                                                                        onClick={() => {this.handleEditToggle(transaction)}}>Edit
-                                                                </button>
-                                                            </td>
-                                                        </tr>
-                                                    )
-                                                } else if (transaction.status === 'Waiting') {
-                                                    return (
-                                                        <tr key={i}>
-                                                            <td>
-                                                                <img src="assets/images/users/user-2.jpg" alt="user"
-                                                                     className="thumb-sm rounded-circle mr-2"/>
-                                                                {transaction.name}
-                                                            </td>
-
-
-                                                            <td><i className="mdi mdi-checkbox-blank-circle text-warning"></i> Awaiting payment</td>
-                                                            <td>
-                                                                ${transaction.amount}
-                                                                <p className="m-0 text-muted font-14">Amount</p>
-                                                            </td>
-                                                            <td>
-                                                                {new Date(transaction.date).toISOString().split('T')[0]}
-                                                                <p className="m-0 text-muted font-14">Date</p>
-                                                            </td>
-                                                            <td>
-                                                                <button type="button"
-                                                                        className="btn btn-secondary btn-sm waves-effect waves-light"
-                                                                        onClick={() => {this.handleEditToggle(transaction)}}>Edit
-                                                                </button>
-                                                            </td>
-                                                        </tr>
-                                                    )
-                                                } else {
-                                                    return (
-                                                        <tr key={i}>
-                                                            <td>
-                                                                <img src="assets/images/users/user-2.jpg" alt="user"
-                                                                     className="thumb-sm rounded-circle mr-2"/>
-                                                                {transaction.name}
-                                                            </td>
-
-
-                                                            <td><i className="mdi mdi-checkbox-blank-circle text-danger"></i> Payment Declined</td>
-                                                            <td>
-                                                                ${transaction.amount}
-                                                                <p className="m-0 text-muted font-14">Amount</p>
-                                                            </td>
-                                                            <td>
-                                                                {new Date(transaction.date).toISOString().split('T')[0]}
-                                                                <p className="m-0 text-muted font-14">Date</p>
-                                                            </td>
-                                                            <td>
-                                                                <button type="button"
-                                                                        className="btn btn-secondary btn-sm waves-effect waves-light"
-                                                                        onClick={() => {this.handleEditToggle(transaction)}}>Edit
-                                                                </button>
-                                                            </td>
-                                                        </tr>
-                                                    )
-                                                }
-
-                                            }
-                                            )}
-                                            </tbody>
-                                        </table>
-                                    </div>
-
-                                </div>
+                <div className="col-xl-6">
+                    <div className="card m-b-20">
+                        <div className="card-body">
+                            <h4 className="mt-0 m-b-30 header-title">Latest Transactions</h4>
+                            <div className="table-responsive">
+                                <table className="table table-vertical">
+                                    <tbody>
+                                    {this.state.transactions.map((transaction, i) => {
+                                        return (
+                                            <tr key={i}>
+                                                <td className="text-left">
+                                                    <img src="assets/images/users/user-2.jpg" alt="user"
+                                                         className="thumb-sm rounded-circle mr-2"/>
+                                                    {transaction.name}
+                                                </td>
+                                                <TransactionStatus
+                                                    statusCode={transaction.status}
+                                                />
+                                                <td>
+                                                    ${transaction.amount}
+                                                    <p className="m-0 text-muted font-14">Amount</p>
+                                                </td>
+                                                <td>
+                                                    {new Date(transaction.date).toISOString().split('T')[0]}
+                                                    <p className="m-0 text-muted font-14">Date</p>
+                                                </td>
+                                                <td>
+                                                    <button type="button"
+                                                            className="btn btn-secondary btn-sm waves-effect waves-light"
+                                                            onClick={() => {this.handleEditToggle(transaction)}}>Edit
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        )
+                                      }
+                                    )}
+                                    </tbody>
+                                </table>
                             </div>
                         </div>
+                    </div>
+                </div>
                     
             );
         }
---
+++ html/projektarbete/src/components/transactionStatus/index.js
@@ -0,0 +1,25 @@
+
+import React, { Component } from "react";
+
+class TransactionStatus extends Component {
+
+    componentDidMount() {
+    }
+
+    render() {
+
+        if(this.props.statusCode === 'Confirmed'){
+            return <td className="text-left"><i className="mdi mdi-checkbox-blank-circle text-success"></i>Confirm</td>
+        } else if(this.props.statusCode === 'Waiting'){
+            return <td className="text-left"><i className="mdi mdi-checkbox-blank-circle text-warning"></i>Awaiting Payment</td>
+        } else {
+            return <td className="text-left"><i className="mdi mdi-checkbox-blank-circle text-danger"></i>Payment Declined</td>
+        }
+    }
+
+                    
+
+
+}
+
+export default TransactionStatus;
