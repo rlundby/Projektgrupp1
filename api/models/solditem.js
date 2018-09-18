2f0fbd37f2a83958c9e6db536802e5a376c0d67e
rlundby <rickardlundby@gmail.com>
2018-09-18 10:34:00 +0200

    working graph + error fixes

--- api/models/solditem.js
+++ api/models/solditem.js
@@ -1,12 +1,12 @@
 const db = require('mongoose');
 
-const earningsSchema = db.Schema({
+const solditemSchema = db.Schema({
     _id: db.Schema.Types.ObjectId,
     month: { type: Number, required: true },
     year: { type: Number, required: true },
-    marketplace: { type: Number, required: true },
     instore: { type: Number, required: true },
-    total: { type: Number, required: true }
+    download: { type: Number, required: true },
+    marketplace: { type: Number, required: true }
 });
 
-module.exports = db.model('Earnings', earningsSchema);
+module.exports = db.model('Solditem', solditemSchema);
