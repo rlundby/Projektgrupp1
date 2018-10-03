3e682bf1de9092f6cb30115751b0b33261f20af2
linneaagetorp <linnea.agetorp@gmail.com>
2018-10-03 08:51:33 +0200

    remove console.log

--- api/auth/check-auth.js
+++ api/auth/check-auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization;
-        console.log('token', token)
+
         const decoded = jwt.verify(token, process.env.PRIVATE_SECRET_KEY);
 
         req.userData = decoded;
